a3f55cb550a604ec3815726daff2e2ed164c876e
Hiroshi Umehara <Hiroshi Umehara>
2013-03-17 17:55:50 +0900

    C#のプロジェクトを追加

---
+++ SerialCommnunication_CSharp/SerialCommnunication_CSharp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SerialCommnunication_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.openButton = new System.Windows.Forms.Button();
+            this.portselectComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Serial Port:";
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(12, 52);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(180, 19);
+            this.messageTextBox.TabIndex = 8;
+            // 
+            // writeButton
+            // 
+            this.writeButton.Enabled = false;
+            this.writeButton.Location = new System.Drawing.Point(198, 48);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(75, 23);
+            this.writeButton.TabIndex = 7;
+            this.writeButton.Text = "Write";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(12, 86);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(260, 166);
+            this.logTextBox.TabIndex = 6;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // openButton
+            // 
+            this.openButton.Enabled = false;
+            this.openButton.Location = new System.Drawing.Point(198, 10);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 11;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // portselectComboBox
+            // 
+            this.portselectComboBox.FormattingEnabled = true;
+            this.portselectComboBox.Location = new System.Drawing.Point(71, 12);
+            this.portselectComboBox.Name = "portselectComboBox";
+            this.portselectComboBox.Size = new System.Drawing.Size(121, 20);
+            this.portselectComboBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.portselectComboBox);
+            this.Name = "Form1";
+            this.Text = "SerialCommunication";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.ComboBox portselectComboBox;
+    }
+}
+
---
+++ SerialCommnunication_CSharp/SerialCommnunication_CSharp/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialCommnunication_CSharp
+{
+
+    public partial class Form1 : Form
+    {
+        // スレッド間処理のために用意
+        private delegate void Delegate_write(string data);
+
+        public Form1()
+        {
+            InitializeComponent();
+            setSerialComboBox();
+        }
+        /****************************************************************************/
+        /*!
+         * シリアルポートを選択するComboBoxを作ります。
+         * 接続されているポートの名前を取得し、表示します。
+         */
+        private void setSerialComboBox()
+        {
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                portselectComboBox.Items.Add(portName);
+            }
+            if (portselectComboBox.Items.Count > 0)
+            {
+                portselectComboBox.SelectedIndex = 0;
+                openButton.Enabled = true;
+            }
+        }
+        /****************************************************************************/
+        /*!
+         * openButtonが押されると実行されます。
+         * 接続の分岐をします。
+         */
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (openButton.Text == "Open")
+            {
+                serialPortOpen();
+                openButton.Text = "Close";
+            }
+            else
+            {
+                serialPortClose();
+                openButton.Text = "Open";
+            }
+        }
+        /****************************************************************************/
+        /*!
+         * 接続を開始します。
+         */
+        private void serialPortOpen()
+        {
+            string portName = portselectComboBox.SelectedItem.ToString();
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = portName;
+            serialPort1.Open();
+            writeButton.Enabled = true;
+        }
+        /****************************************************************************/
+        /*!
+         * 接続を終了します。
+         */
+        private void serialPortClose()
+        {
+            serialPort1.Close();
+            writeButton.Enabled = false;
+        }
+        /****************************************************************************/
+        /*!
+         * writeButtonが押されると実行されます。
+         * serialPort1に送信します。
+         */
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            String text = messageTextBox.Text;
+            serialPort1.Write(text);
+            //Console.WriteLine(text);
+            // messageTextBoxをクリア
+            messageTextBox.Text = "";
+        }
+
+        /****************************************************************************/
+        /*!
+         * serialPort1でデータを受信すると実行されます。
+         */
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // 受信したデータ
+            string data = serialPort1.ReadLine();
+            // 異なるスレッドのテキストボックスに書き込む
+            Invoke(new Delegate_write(write), new Object[] { data });
+        }
+
+        /****************************************************************************/
+        /*!
+         * logTextBoxに受信内容を書き込みます。
+         */
+        private void write(string data)
+        {
+            if (data != null)
+            {
+                logTextBox.AppendText(data + "\n");
+            }
+        }
+
+        /****************************************************************************/
+        /*!
+         * ウィンドウが閉じられる際に実行されます。
+         */
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // シリアル通信していた場合は、閉じてから終了する
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+    }
+}
